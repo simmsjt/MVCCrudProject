46fb6844266d6f04e99c4877d8c886506009e2d9
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-11-10 22:27:14 -0700

    added new methods to DAO and implemented the in DAOIMpl

--- src/data/CigarDAO.java
+++ src/data/CigarDAO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface CigarDAO {
 	List<Cigar> getAllCigars();
+	List<Cigar> getCigarsByShape(Shape s);
+	List<Cigar> getCigarsByWrapper(WrapperType w);
 	Cigar getCigarById(int id); 
 	Cigar addCigar(Cigar g);
 	Cigar deleteCigar(Cigar g);
---
+++ src/data/CigarDAOImpl.java
@@ -0,0 +1,37 @@
+package data;
+
+import java.util.List;
+
+public class CigarDAOImpl implements CigarDAO {
+
+	@Override
+	public List<Cigar> getAllCigars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cigar getCigarById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cigar addCigar(Cigar g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cigar deleteCigar(Cigar g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cigar editCigar(Cigar g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

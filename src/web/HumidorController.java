9e025600d2705ef6e582c205ae4efe1e8e4e5b53
Jacob Simms <simmsjt@Jacobs-MacBook-Pro.local>
2017-11-10 23:32:54 -0700

    created humidor controller and added home and addcigar method also created index.jsp

---
+++ src/web/HumidorController.java
@@ -0,0 +1,41 @@
+package web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import data.Cigar;
+import data.CigarDAO;
+import data.Shape;
+import data.WrapperType;
+
+@Controller
+public class HumidorController {
+	
+	@Autowired
+	CigarDAO dao;
+	
+	@RequestMapping(path = "home.do", method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		List<Cigar> allCigars = dao.getAllCigars();
+		mv.addObject("list", allCigars);
+		mv.setViewName("index");
+		return mv;
+	}
+	
+	
+	@RequestMapping(path = "addCigar.do", method = RequestMethod.GET)
+	public String addGiraffe(Model model) {
+		Cigar c = new Cigar("Tim", "apples", WrapperType.CLARO, Shape.CHURCHILL);
+		dao.addCigar(c);
+		model.addAttribute("cigar", c);
+		return "add";
+	}
+
+}
